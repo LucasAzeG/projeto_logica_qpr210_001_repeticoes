085e4e3851b2bb1d54bb7099d4519750e84b8dc4
Lucas Azedi <lucas.a.goes@aluno.senai.br>
2026-06-16 16:39:56 -0300

    Aicionando barra de formulario

--- js/scripts repeticoes.js
+++ js/scripts repeticoes.js
@@ -3,4 +3,11 @@ const divFor = document.querySelector('#div-for')
 
 for(i = 0;i < 10; i++ ){
 console.log(i, "Ódio")
+divFor.innerHTML += `${i} - Ódio <br>`
 }
+
+//CONTADOR/ACUMULADOR
+const inputNum = document.querySelector('#num')
+const btnNum = ocument.querySelector('#btn-num')
+const divContAcum = document.querySelector('#div-cont-acum')
+
