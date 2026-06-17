508ab48539d31787def2945612bf3bb11a8c9769
Lucas Azedi <lucas.a.goes@aluno.senai.br>
2026-06-17 14:34:20 -0300

    resolvendo erros

--- js/scripts repeticoes.js
+++ js/scripts repeticoes.js
@@ -14,20 +14,20 @@ const divContAcum = document.querySelector('#div-cont-acum')
 let cont = 0
 let acum = 0.0
 
-btnNum.addEventListener('click', (evt)=>{
+btnNum.addEventListener('click', (evt) => {
     let numDigitado = Number(inputNum.value)
 
     cont++
     acum += numDigitado
 
-    divContAcum.innerHTML = `TOTAL DE NÚMERO DIGITADO É: ${CONT} <br} A SOMA DOS NÚMEROS É: {acum}`
+    divContAcum.innerHTML = `TOTAL DE NÚMERO DIGITADO É: ${cont} <br} A SOMA DOS NÚMEROS É: ${acum}`
 
     inputNum.value = ''
 
     })
 
     //CONTROLADO FOR
-    const inputFrase = document.querySelector('frase')
+    const inputFrase = document.querySelector('#frase')
     const inputNumRepeticao = document.querySelector('#num-repeticao')
     const bntFrase = document.querySelector('#btn-frase')
     const divFrase= document.querySelector('#div-frase')
@@ -41,3 +41,4 @@ btnNum.addEventListener('click', (evt)=>{
         }
     })
 
+//FOR IN
